e3a19de36fc8cdafc5070c299d7587defbe74182
Marcelo Barsotti <marcelo.barsotti@assentcompliance.com>
2019-08-23 11:37:43 -0400

    13. Andular Modules

---
+++ APM-Start/src/app/products/product.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { ProductDetailGuard } from './product-detail.guard';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent
+  ],
+  imports: [
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', 
+      canActivate: [ProductDetailGuard],
+      component: ProductDetailComponent }
+    ]),
+    SharedModule
+  ]
+})
+export class ProductModule { }
